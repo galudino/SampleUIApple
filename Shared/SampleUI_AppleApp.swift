180859bb11393368c03a9bfbdc55afcd8010c968
Gemuele Aludino <g.aludino@gmail.com>
2021-08-01 16:17:24 -0400

    Initial Commit

---
+++ Shared/SampleUI_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleUI_AppleApp.swift
+//  Shared
+//
+//  Created by Gemuele Aludino on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleUI_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
